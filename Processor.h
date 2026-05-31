ce0eb7f961f8f8137d808d7c154943a48e96dcad
Mick-Barnett <156118755+Mick-Barnett@users.noreply.github.com>
2026-05-30 19:57:24 -0700

    Update Processor.h

--- Processor.h
+++ Processor.h
@@ -1,1 +1,25 @@
 
+
+/*
+ * Processor.h
+ *
+ *  Created on: May 29, 2026
+ *      Author: mickp
+ */
+
+#ifndef PROCESSOR_H_
+#define PROCESSOR_H_
+
+typedef enum {
+    E2 = 0, A2, D3, G3, B3, E4
+} NoteIndex;
+
+extern NoteIndex note;
+extern float cents;
+extern float percent_error;
+
+void Processor_init(void);
+void Processor(float frequency);
+
+#endif /* PROCESSOR_H_ */
+
