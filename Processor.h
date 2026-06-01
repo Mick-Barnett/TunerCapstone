eba89efe2d0abce5cef4f27683f3a446ac0dd172
Mick-Barnett <156118755+Mick-Barnett@users.noreply.github.com>
2026-06-01 11:14:36 -0700

    Update Processor.h
    Updated the note configurations and made cents and note more global for accessibility in the other modules. Idk if I'd pasted it in properly the first time I pushed.

--- Processor.h
+++ Processor.h
@@ -1,18 +1,29 @@
 
-
 /*
  * Processor.h
  *
  *  Created on: May 29, 2026
  *      Author: mickp
  */
-
 #ifndef PROCESSOR_H_
 #define PROCESSOR_H_
 
 typedef enum {
-    E2 = 0, A2, D3, G3, B3, E4
+    E2 = 0,
+    A2,
+    D3,
+    G3,
+    B3,
+    E4,
+    NOTE_COUNT
 } NoteIndex;
+
+typedef struct {
+    const char *name;
+    float frequency;
+} Note;
+
+extern const Note notes[NOTE_COUNT];
 
 extern NoteIndex note;
 extern float cents;
@@ -21,5 +32,5 @@ extern float percent_error;
 void Processor_init(void);
 void Processor(float frequency);
 
-#endif /* PROCESSOR_H_ */
+#endif
 
