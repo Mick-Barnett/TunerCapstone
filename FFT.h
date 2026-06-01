6046da5d5e0ee61523d4fb5cf0eaf78494bfa09c
Aaron Price Jr <156409292+aaronpricejr@users.noreply.github.com>
2026-06-01 10:42:19 -0700

    Update FFT.h

--- FFT.h
+++ FFT.h
@@ -2,7 +2,8 @@
  ******************************************************************************
  * @file    : FFT.h
  * @brief   : Header for FFT module. Contains function prototypes for obtaining
- * 			  frequency, noise filtering, and choosing a magnitude for harmonics
+ * 			  frequency, noise filtering, and choosing a magnitude peak for
+ * 			  harmonics.
  * project  : EE 329 S'26 A5
  * authors  : Aaron Price Jr.
  * version  : 0.2
@@ -11,20 +12,34 @@
  ******************************************************************************
  */
 
-#ifndef INC_ADC_H_
-#define INC_ADC_H_
-
-#define f_sample 8000 //8kHz sample rate
-#define FFT_size 2048 // 2048 samples
-
+#ifndef INC_FFT_H_
+#define INC_FFT_H_
 
 #include "stm32l4xx.h"
 #include "ADC.h"
 #include <stdint.h>
+#include <math.h>
 
+#define f_sample 8000 //8kHz sample rate
+#define FFT_size 2048 // 2048 samples
+#ifndef M_PI
+#define M_PI 3.14159265358979323846f
+#endif
+
+
+/* Creates an object class for real and imaginary components for the FFT.
+ * x = real + j(imag)
+ */
+typedef struct
+{
+	float real;
+	float imag;
+} complex_t;
+
+//-------------------------FUNCTION PROTOTYPES----------------------------------
 void FFT_Init(void);
-void FFT_Get_Freq(uint16_t adc_buffer);
-void FFT_Noise_Filter(void);
+void FFT_Get_Freq(uint16_t adc_buffer, uint32_t length);
+void FFT_Noise_Filter(float magnitudes, uint32_t length);
 void FFT_Get_Last_Magnitude();
 
-#endif /* INC_ADC_H_ */
+#endif /* INC_FFT_H_ */
