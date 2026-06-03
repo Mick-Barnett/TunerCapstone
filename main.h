6be0c370a2096fbf21321ad4e93029ed6c58318a
Aaron Price Jr <156409292+aaronpricejr@users.noreply.github.com>
2026-06-02 20:31:12 -0700

    Update main.h

--- main.h
+++ main.h
@@ -1,1 +1,23 @@
 
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+#include "stm32l4xx.h"
+#include "ADC.h"
+#include "FFT.h"
+#include "lpuart.h"
+#include "delay.h"
+
+void Error_Handler(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
