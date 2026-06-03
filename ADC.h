d0fda84e7065010db0da6c96d49222741f893def
Aaron Price Jr <156409292+aaronpricejr@users.noreply.github.com>
2026-06-02 20:30:11 -0700

    Refactor ADC header file for clarity and organization

--- ADC.h
+++ ADC.h
@@ -1,25 +1,46 @@
+/**
+ ******************************************************************************
+ * @file    : ADC.h
+ * @brief   : Header for ADC module
+ * project  : EE 329 S'26 A5
+ * authors  : Aaron Price Jr. & Brandon Valenti
+ * version  : 0.2
+ * date     : 2026-05-19
+ * target   : NUCLEO-L4A6ZG
+ *
+ * ADC1 channel 5, PA0 (Nucleo A0)
+ * 12-bit resolution, single conversion, software trigger
+ * Sample time: 47.5 clock cycles (SMPR1 SMP5 = 0b100)
+ * VREF = 3.3V (internal)
+ ******************************************************************************
+ */
 #ifndef INC_ADC_H_
 #define INC_ADC_H_
 
-#include "stm32l4xx_hal.h"
+#include "stm32l4xx.h"
 #include "delay.h"
 #include <stdint.h>
 
-// ch 5 Sample Time: 1 = 6.5 clocks, 4 = 47.5 clocks, 7 = 640.5 clocks
-#define SAMPLE_TIME 1 
-// DSP Constants
-#define ADC_BUFF_SIZE 4096
-#define ADC_WND_SIZE 2048
-#define ADC_HALF_WND 1024
+/* ADC sample time selection */
+#define SAMPLE_TIME    1
 
-static volatile uint16_t ADC_buffer[ADC_BUFF_SIZE];
-static volatile uint16_t ADC_buff_idx;
-static volatile uint16_t ADC_samp_cnt;
-static volatile uint16_t ADC_ref_idx;
+/* DSP constants */
+#define ADC_BUFF_SIZE  4096
+#define ADC_WND_SIZE   2048
+#define ADC_HALF_WND   1024
+
+/* Global ADC state */
+extern volatile uint16_t ADC_buffer[ADC_BUFF_SIZE];
+extern volatile uint16_t ADC_buff_idx;
+extern volatile uint16_t ADC_samp_cnt;
+extern volatile uint16_t ADC_ref_idx;
 extern volatile uint8_t ADC_wndw_ready;
 
+/* Public functions */
 void ADC_init(void);
-void ADC1_2_IRQHandler(void);
-void ADC_GetWindow(uint16_t window[SIZE]);
 
-#endif
+void ADC_GetWindow(uint16_t window[ADC_WND_SIZE]);
+
+void ADC1_2_IRQHandler(void);
+
+#endif /* INC_ADC_H_ */
